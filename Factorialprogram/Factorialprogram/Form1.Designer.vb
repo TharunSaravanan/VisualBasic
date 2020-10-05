b88c64e1fb21023db49802fdb8ea5bd055c632fe
TharunSaravanan <bstharun@gmail.com>
2020-10-04 19:19:17 -0500

    Factorial Program

---
+++ Factorialprogram/Factorialprogram/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFact = New System.Windows.Forms.TextBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.lblNumber = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtFact
+        '
+        Me.txtFact.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtFact.Location = New System.Drawing.Point(12, 0)
+        Me.txtFact.Name = "txtFact"
+        Me.txtFact.Size = New System.Drawing.Size(335, 50)
+        Me.txtFact.TabIndex = 0
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Font = New System.Drawing.Font("Segoe UI", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEnter.Location = New System.Drawing.Point(12, 56)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(335, 61)
+        Me.btnEnter.TabIndex = 1
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'lblNumber
+        '
+        Me.lblNumber.Font = New System.Drawing.Font("Segoe UI", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumber.Location = New System.Drawing.Point(586, 346)
+        Me.lblNumber.Name = "lblNumber"
+        Me.lblNumber.Size = New System.Drawing.Size(202, 95)
+        Me.lblNumber.TabIndex = 2
+        Me.lblNumber.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblNumber)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.txtFact)
+        Me.Name = "Form1"
+        Me.Text = "Factorial"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFact As TextBox
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents lblNumber As Label
+End Class
---
+++ Factorialprogram/Factorialprogram/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Dim fact As Long
+    Dim n As Long
+    Dim intCount As Long
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        fact = 1
+        n = Integer.Parse(txtFact.Text)
+        For intCount = 1 To n
+            fact = fact * intCount
+
+        Next
+        lblNumber.Text = fact
+    End Sub
+End Class
