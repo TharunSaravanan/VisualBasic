3f4ae5cb3961a74d8e8c619111bf1c6f75e67043
TharunSaravanan <bstharun@gmail.com>
2020-10-18 19:35:31 -0500

    Program to find if a number is bigger or smaller

---
+++ BigOrSmall/BigOrSmall/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFirst = New System.Windows.Forms.TextBox()
+        Me.txtSecond = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtFirst
+        '
+        Me.txtFirst.Location = New System.Drawing.Point(105, 80)
+        Me.txtFirst.Name = "txtFirst"
+        Me.txtFirst.Size = New System.Drawing.Size(100, 20)
+        Me.txtFirst.TabIndex = 0
+        '
+        'txtSecond
+        '
+        Me.txtSecond.Location = New System.Drawing.Point(688, 80)
+        Me.txtSecond.Name = "txtSecond"
+        Me.txtSecond.Size = New System.Drawing.Size(100, 20)
+        Me.txtSecond.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(12, 83)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 23)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "First Number:"
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(577, 83)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 23)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Second Number:"
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(366, 344)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 4
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(220, 232)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(369, 45)
+        Me.Label4.TabIndex = 6
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtSecond)
+        Me.Controls.Add(Me.txtFirst)
+        Me.Name = "Form1"
+        Me.Text = "Bigger or Smaller"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFirst As TextBox
+    Friend WithEvents txtSecond As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents Label4 As Label
+End Class
---
+++ BigOrSmall/BigOrSmall/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Dim Num1 As Integer
+    Dim Num2 As Integer
+    Private Sub btnCalc_Click_1(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Dim Num1 As Integer = Integer.Parse(txtFirst.Text)
+        Dim Num2 As Integer = Integer.Parse(txtSecond.Text)
+
+        If Num1 > Num2 Then
+            Label4.Text = (Num1) & " (first number) is bigger than " & (Num2) & " (second number)"
+        Else
+            Label4.Text = (Num2) & " (second number) is bigger than " & (Num1) & " (first number)"
+        End If
+
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
